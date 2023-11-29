837470b6442502407dece1d7807953faeb165fca
Ayobamee <ayobami.elutade@com>
2023-11-29 17:01:14 +0100

    restyled and seperated components

---
+++ src/CustomDropdown.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+
+function CustomDropdown({ employeeType, onSelect }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleDropdown = () => setIsOpen(!isOpen);
+
+  const handleSelect = (value) => {
+    onSelect(value);
+    setIsOpen(false);
+  };
+
+  return (
+    <div className="custom-dropdown">
+      <button className="drop-btn" onClick={toggleDropdown}>
+        {employeeType || "Select Employee Type"}
+      </button>
+      {isOpen && (
+        <div className="dropdown-content">
+          <a href="#!" onClick={() => handleSelect("Founder")}>
+            Founder
+          </a>
+          <a href="#!" onClick={() => handleSelect("Permanent")}>
+            Permanent
+          </a>
+          <a href="#!" onClick={() => handleSelect("Contract")}>
+            Contract
+          </a>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default CustomDropdown;
